1a8781406194fdf48bb741bd99f36a5731ba6ab7
chenzhizsqq <chenzhi@qq.com>
2023-04-19 14:15:25 +0900

    Initial Commit

---
+++ SelfLearnSpeak/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SelfLearnSpeak
+//
+//  Created by chenzhizs on 2023/04/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SelfLearnSpeak/SelfLearnSpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfLearnSpeakApp.swift
+//  SelfLearnSpeak
+//
+//  Created by chenzhizs on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct SelfLearnSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
