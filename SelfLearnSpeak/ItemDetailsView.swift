6099fa3d8b7b371b056c231dd5d2c519fb347ade
chenzhizsqq <chenzhi@qq.com>
2023-04-21 19:18:44 +0900

    ItemDetailsView - 画面改了，但是那个翻译按钮有问题

--- SelfLearnSpeak/ItemDetailsView.swift
+++ SelfLearnSpeak/ItemDetailsView.swift
@@ -13,32 +13,80 @@ import AVFoundation
 /// Represents a screen where you can edit the item's name.
 struct ItemDetailsView: View {
     @ObservedRealmObject var item: Item
+    @StateObject var transViewModel = TransViewModel()
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("原文:")
-            // Accept a new name
-            TextEditor(text: $item.name)
-                .autocapitalization(.none)
-                .frame(height: 150)
-                .overlay(RoundedRectangle(cornerRadius: 5).stroke(.gray, lineWidth: 1))
-                .border(.gray)
-                .background(.cyan)
-                .padding()
-            
-            
-                Text("翻译后:")
-            TextEditor(text: $item.itemDescription)
-                .autocapitalization(.none)
-                .frame(height: 150)
-                .overlay(RoundedRectangle(cornerRadius: 5).stroke(.gray, lineWidth: 1))
-                .border(.gray)
-                .background(.cyan)
-                .padding()
-        }.padding()
-            .navigationBarTitle(item.name)
+        ScrollView{
+            VStack {
+                HStack{
+                    Text("日文")
+                        .textFieldStyle(DefaultTextFieldStyle())
+                        .padding(.horizontal)
+                    Button("翻译") {
+                        if(!item.name.isEmpty){
+                            BaiduFanyiAPI.getfanyi(from: "jp" ,to: "zh", text: item.name, type: "jp2zh", mvvm: transViewModel)
+                        }else{
+                            debugPrint("!!! input_text.isEmpty")
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                
+                // Accept a new name
+                TextEditor(text: $item.name)
+                    .autocapitalization(.none)
+                    .frame(height: 150)
+                    .overlay(RoundedRectangle(cornerRadius: 5).stroke(.gray, lineWidth: 1))
+                    .border(.gray)
+                    .background(.cyan)
+                    .padding()
+                
+                HStack{
+                    Text("中文")
+                        .textFieldStyle(DefaultTextFieldStyle())
+                        .padding(.horizontal)
+                    Button("翻译") {
+                        if(!item.itemDescription.isEmpty){
+                            BaiduFanyiAPI.getfanyi(from: "zh" ,to: "jp", text: item.itemDescription, type: "zh2jp", mvvm: transViewModel)
+                        }else{
+                            debugPrint("!!! description_text.isEmpty")
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                
+                
+                TextEditor(text: $item.itemDescription)
+                    .autocapitalization(.none)
+                    .frame(height: 150)
+                    .overlay(RoundedRectangle(cornerRadius: 5).stroke(.gray, lineWidth: 1))
+                    .border(.gray)
+                    .background(.cyan)
+                    .padding()
+            }
+        }
+        .onTapGesture {
+            hideKeyboard()
+        }
+        .onChange(of: transViewModel.jp) { newValue in
+            // 这里可以响应myValue变化时的操作
+            debugPrint("transViewModel.jp")
+            item.name = newValue
+            debugPrint(newValue)
+        }
+        .onChange(of: transViewModel.zh) { newValue in
+            // 这里可以响应myValue变化时的操作
+            debugPrint("transViewModel.zh")
+            item.itemDescription = newValue
+            debugPrint(newValue)
+        }
+        .padding()
             .navigationBarItems(trailing: Toggle(isOn: $item.isFavorite) {
                 Image(systemName: item.isFavorite ? "heart.fill" : "heart")
             })
+    }
+    
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
