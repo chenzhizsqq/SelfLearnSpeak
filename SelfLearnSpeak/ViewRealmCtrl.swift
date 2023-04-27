d492a851bc76cc262ea1511aa482ba322bfe5c4f
chenzhizsqq <chenzhi@qq.com>
2023-04-27 18:15:03 +0900

    优化了一点

--- SelfLearnSpeak/ViewRealmCtrl.swift
+++ SelfLearnSpeak/ViewRealmCtrl.swift
@@ -13,6 +13,7 @@ import RealmSwift
 struct ViewRealmCtrl: View {
     @StateObject var viewModel = ViewModel()
     @State var selectedTable: String?
+    //@State var res: String
     
     init(){
         Realm.Configuration(deleteRealmIfMigrationNeeded: true)
@@ -20,12 +21,40 @@ struct ViewRealmCtrl: View {
 
     var body: some View {
         VStack {
-            List(viewModel.tables, id: \.self) { table in
+            List(viewModel.tables, id: \.self, selection: $selectedTable) { table in
                 Text(table)
+            }
+            Button("结构") {
+                if let selectedTable = selectedTable {
+                    let realm = try! Realm()
+                    let objectSchema = realm.schema.objectSchema.first(where: { $0.className == selectedTable })
+                    let propertyList = objectSchema?.properties.map({ $0.name })
+                    print(propertyList)
+                    if let propertyList = propertyList as? [String] {
+                        // `res` is now of type `[String]`
+                        // Do something with `res` here...
+                        //res = propertyList.joined(separator: ", ")
+                    }
+
+                }
+            }
+            Button("内容") {
+                if let selectedTable = selectedTable {
+                    let realm = try! Realm()
+                    let objectSchema = realm.schema.objectSchema.first(where: { $0.className == selectedTable })
+                    let propertyList = objectSchema?.description.map({ $0.description })
+                    
+                    let combinedString = propertyList!.joined(separator: "")
+                    print(combinedString)
+
+                }
             }
             List(viewModel.tablesDescription, id: \.self) { table in
                 Text(table)
             }
+            
+                //Text(res)
+            
             Button("Delete All Tables") {
                 viewModel.deleteAllTables()
             }
@@ -40,6 +69,13 @@ class ViewModel: ObservableObject {
     }
     var tablesDescription: [String] {
         return realm.schema.objectSchema.map { $0.description }
+    }
+    
+    func printTableData(_ tableName: String) {
+        guard let table = realm.objects(Object.self).filter("className = '\(tableName)'").first?.description else {
+            return
+        }
+        print(table)
     }
 
     func deleteAllTables() {
